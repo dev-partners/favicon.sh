c28d1e65bb4d59502039f232f25304eb7d85c0c5
Matt Watson <matt@recycledbeans.com>
2020-01-23 11:09:00 -0500

    Create favicon.sh

---
+++ favicon.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sizes=( 32 128 152 167 180 192 196 )
+
+if [ -z "$1" ]
+  then
+    echo "The first argument should be the file name"
+    exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "The second argument should be the output location (directory)"
+    exit 1
+fi
+
+mkdir -p $2
+
+for i in "${sizes[@]}"
+do
+    :
+    convert $1 -resize $ix$i $2/favicon-$i.png
+done
